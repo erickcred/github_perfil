05cce5f7ed85ba8f14e6319fe042b0f16cd3589b
Erick Rick <erickcredjes@gmail.com>
2023-03-09 10:33:43 -0300

    Removendo formulário

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,8 @@
 import { useState } from 'react';
 import Perfil from './components/Perfil';
-import Formulario from './components/Formulario';
 import ReposList from './components/ReposList';
 
 function App() {
-  const [formularioEstaVisivel, setFormularioEstaVisivel] = useState(true);
   const [nomeUsuario, setNomeUsuario] = useState("");
 
   return (
@@ -18,11 +16,6 @@ function App() {
           <ReposList nomeUsuario={nomeUsuario}/>
         </>
       )}
-
-      {/* <button type="button" onClick={ () => setFormularioEstaVisivel(!formularioEstaVisivel) }>toggle form</button>
-      {formularioEstaVisivel && (
-        <Formulario />
-      )} */}
 
     </div>
   );
