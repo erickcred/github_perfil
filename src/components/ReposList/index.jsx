51236efa57c9b13ffcbeb10baa0859c072525356
Erick Rick <erickcredjes@gmail.com>
2023-03-09 11:00:12 -0300

    Implementado tratamento de exceção

--- src/components/ReposList/index.jsx
+++ src/components/ReposList/index.jsx
@@ -4,23 +4,65 @@ import styles from './ReposList.module.css';
 const ReposList = ({ nomeUsuario }) => {
     const [repos, setRepos] = useState([]);
     const [estaCarregando, setEstaCarregando] = useState(true);
+    const [erro, setErro] = useState(false);
 
     useEffect(() => {
         setEstaCarregando(true);
+        setErro(false);
 
         fetch(`https://api.github.com/users/${nomeUsuario}/repos`)
-            .then(res => res.json())
+            .then(res => {
+                if (!res.ok) throw new Error();
+                return res.json();
+            })
             .then(resJson => {
                 setTimeout(() => {
                     setEstaCarregando(false);             
                     setRepos(resJson);
                 }, 1500);
+            })
+            .catch(e => {
+                setErro(true);
             });
-    }, [nomeUsuario])
+    }, [nomeUsuario]);
 
     return (
         <div className="container">
-            {estaCarregando ? (
+
+            {
+                erro ? (
+                    <h1>Ops! Node de usuário informado não está correto.</h1>
+                ) : (
+                    estaCarregando ? (
+                        <h1>Carregando...</h1>
+                    ) : (
+                        <ul className={styles.list}>
+                            {
+                                repos.map(repositorio => (
+                                    <li key={repositorio.id} className={styles.listItem}>
+                                        <div className={styles.itemName}>
+                                            <b>Nome:</b>{repositorio.name} 
+                                        </div>
+                                        <div className={styles.itemLanguage}>
+                                            <b>Linguagem:</b>{repositorio.language}
+                                        </div>
+                                        <div>
+                                            <a className={styles.itemLink} href={repositorio.html_url} target="_blank">Acessar repositório</a>
+                                        </div>
+                                        {/* <div className={styles.itemDate}>
+                                            <b>Criado:</b> 
+                                            <time>
+                                                {new Date(repositorio.created_at).toLocaleString("pt-br", {timezone: "UTC"})}
+                                            </time>
+                                        </div> */}
+                                    </li>
+                                ))
+                            }
+                        </ul>
+                    )
+                )
+            }
+            {/* {estaCarregando ? (
                 <h1>Carregando...</h1>
             ) : (
                 <ul className={styles.list}>
@@ -36,17 +78,17 @@ const ReposList = ({ nomeUsuario }) => {
                                 <div>
                                     <a className={styles.itemLink} href={repositorio.html_url} target="_blank">Acessar repositório</a>
                                 </div>
-                                {/* <div className={styles.itemDate}>
+                                <div className={styles.itemDate}>
                                     <b>Criado:</b> 
                                     <time>
                                         {new Date(repositorio.created_at).toLocaleString("pt-br", {timezone: "UTC"})}
                                     </time>
-                                </div> */}
+                                </div>
                             </li>
                         ))
                     }
                 </ul>
-            )}
+            )} */}
         </div>
     )
 }
